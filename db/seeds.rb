c666ae6b5dcba594380f92b5f55e05983fea8a80
Cori Allen <coriallen@gmail.com>
2013-10-15 15:02:45 -0400

    Adds actions.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Spider.create(name: "yikes", poisonous: true, weight: 1.2, habitat: 'basement', terror: 9, origin: 'under a rock')
+Spider.create(name: "blerg", poisonous: true, weight: 0.4, habitat: 'bathtub', terror: 8, origin: 'under a rock')
+Spider.create(name: "nerds", poisonous: true, weight: 2, habitat: 'garden', terror: 3, origin: 'under a rock')
+Spider.create(name: "zoinks", poisonous: true, weight: 19, habitat: 'moldy old file cabinet', terror: 10, origin: 'under a rock')
